98b84c157320cabad9baf8eedfff4a4003da1822
Shunqian Zheng <zhengsq@rock-chips.com>
2021-09-23 09:51:24 +0800

    rkaiq_3A_server: get sensor entity name from librkaiq
    The entity name can be read from librkaiq.so. With this changes,
    rkaiq_3A_server supported the media topology that link rkisp to rkcif
    interface.
    
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
    Change-Id: I91ad60fd329ca8df361e71a99e70fe5947617100

--- rkaiq_3A_server/rkaiq_3A_server.cpp
+++ rkaiq_3A_server/rkaiq_3A_server.cpp
@@ -24,8 +24,9 @@
                     (V4L2_EVENT_PRIVATE_START + 2)
 
 #define RKAIQ_FILE_PATH_LEN                       64
-#define RKAIQ_CAMS_NUM_MAX                        2
 #define RKAIQ_FLASH_NUM_MAX                       2
+
+#define IQ_PATH "/etc/iqfiles/"
 
 rk_aiq_sys_ctx_t* aiq_ctx = NULL;
 static int silent = 0;
@@ -37,14 +38,8 @@ struct rkaiq_media_info {
     char sd_isp_path[RKAIQ_FILE_PATH_LEN];
     char vd_params_path[RKAIQ_FILE_PATH_LEN];
     char vd_stats_path[RKAIQ_FILE_PATH_LEN];
-
-    struct {
-            char sd_sensor_path[RKAIQ_FILE_PATH_LEN];
-            char sd_lens_path[RKAIQ_FILE_PATH_LEN];
-            char sd_flash_path[RKAIQ_FLASH_NUM_MAX][RKAIQ_FILE_PATH_LEN];
-            bool link_enabled;
-            char sensor_entity_name[32];
-    } cams[RKAIQ_FLASH_NUM_MAX];
+    char mainpath[RKAIQ_FILE_PATH_LEN];
+    char sensor_entity_name[32];
 };
 
 static struct rkaiq_media_info media_info;
@@ -70,132 +65,69 @@ static int xioctl(int fh, int request, void *arg)
 
 static int rkaiq_get_devname(struct media_device *device, const char *name, char *dev_name)
 {
-	const char *devname;
-	struct media_entity *entity =  NULL;
+    const char *devname;
+    struct media_entity *entity =  NULL;
 
-	entity = media_get_entity_by_name(device, name, strlen(name));
-	if (!entity)
-		return -1;
+    entity = media_get_entity_by_name(device, name, strlen(name));
+    if (!entity)
+        return -1;
 
-	devname = media_entity_get_devname(entity);
+    devname = media_entity_get_devname(entity);
 
-	if (!devname) {
-		fprintf(stderr, "can't find %s device path!", name);
-		return -1;
-	}
+    if (!devname) {
+        fprintf(stderr, "can't find %s device path!", name);
+        return -1;
+    }
 
-	strncpy(dev_name, devname, RKAIQ_FILE_PATH_LEN);
+    strncpy(dev_name, devname, RKAIQ_FILE_PATH_LEN);
 
-	DBG("get %s devname: %s\n", name, dev_name);
+    DBG("get %s devname: %s\n", name, dev_name);
 
-	return 0;
+    return 0;
 }
-	
-static int rkaiq_enumrate_modules(struct media_device *device, struct rkaiq_media_info *media_info)
-{
-	uint32_t nents, i;
-	const char* dev_name = NULL;
-	int active_sensor = -1;
 
-	nents = media_get_entities_count (device);
-	for (i = 0; i < nents; ++i) {
-		int module_idx = -1;
-		struct media_entity *e;
-		const struct media_entity_desc *ef;
-		const struct media_link *link;
-
-		e = media_get_entity(device, i);
-		ef = media_entity_get_info(e);
-
-		if (ef->type != MEDIA_ENT_T_V4L2_SUBDEV_SENSOR &&
-			ef->type != MEDIA_ENT_T_V4L2_SUBDEV_FLASH &&
-			ef->type != MEDIA_ENT_T_V4L2_SUBDEV_LENS)
-			continue;
-
-		if (ef->name[0] != 'm' && ef->name[3] != '_') {
-			fprintf(stderr, "sensor/lens/flash entity name format is incorrect,"
-							"pls check driver version !\n");
-			return -1;
-		}
-
-		/* Retrive the sensor index from sensor name,
-		  * which is indicated by two characters after 'm',
-		  *	 e.g.  m00_b_ov13850 1-0010
-		  *			^^, 00 is the module index
-		  */
-		module_idx = atoi(ef->name + 1);
-		if (module_idx >= RKAIQ_CAMS_NUM_MAX) {
-			fprintf(stderr, "sensors more than two not supported, %s\n",
-					ef->name);
-			continue;
-		}
-
-		dev_name = media_entity_get_devname (e);
-
-		switch (ef->type) {
-		case MEDIA_ENT_T_V4L2_SUBDEV_SENSOR:
-			strncpy(media_info->cams[module_idx].sd_sensor_path,
-					dev_name, RKAIQ_FILE_PATH_LEN);
-
-			link = media_entity_get_link(e, 0);
-			if (link && (link->flags & MEDIA_LNK_FL_ENABLED)) {
-				media_info->cams[module_idx].link_enabled = true;
-				active_sensor = module_idx;
-				strcpy(media_info->cams[module_idx].sensor_entity_name, ef->name);
-			}
-			break;
-		case MEDIA_ENT_T_V4L2_SUBDEV_FLASH:
-			// TODO, support multiple flashes attached to one module
-			strncpy(media_info->cams[module_idx].sd_flash_path[0],
-					dev_name, RKAIQ_FILE_PATH_LEN);
-			break;
-		case MEDIA_ENT_T_V4L2_SUBDEV_LENS:
-			strncpy(media_info->cams[module_idx].sd_lens_path,
-					dev_name, RKAIQ_FILE_PATH_LEN);
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (active_sensor < 0) {
-		fprintf(stderr, "Not sensor link is enabled, does sensor probe correctly?\n");
-		return -1;
-	}
-
-	return 0;
-}
-	
 int rkaiq_get_media_info(struct rkaiq_media_info *media_info, const char *mdev_path) 
 {
-	struct media_device *device = NULL;
-	int ret;
+    struct media_device *device = NULL;
+    const char *sensor_name;
+    int ret;
 
-	device = media_device_new (mdev_path);
-	if (!device)
-		return -ENOMEM;
-	/* Enumerate entities, pads and links. */
-	ret = media_device_enumerate (device);
-	if (ret)
-		return ret;
-	if (!ret) {
-		/* Try rkisp */
-		ret = rkaiq_get_devname(device, "rkisp-isp-subdev",
-								media_info->sd_isp_path);
-		ret |= rkaiq_get_devname(device, "rkisp-input-params",
-								media_info->vd_params_path);
-		ret |= rkaiq_get_devname(device, "rkisp-statistics",
-								media_info->vd_stats_path);
-	}
-	if (ret) {
-		media_device_unref (device);
-		return ret;
-	}
+    device = media_device_new (mdev_path);
+    if (!device)
+        return -ENOMEM;
+    /* Enumerate entities, pads and links. */
+    ret = media_device_enumerate (device);
+    if (ret)
+        return ret;
+    if (!ret) {
+        /* Try rkisp */
+        ret = rkaiq_get_devname(device, "rkisp-isp-subdev",
+                                media_info->sd_isp_path);
+        ret |= rkaiq_get_devname(device, "rkisp-input-params",
+                                media_info->vd_params_path);
+        ret |= rkaiq_get_devname(device, "rkisp-statistics",
+                                media_info->vd_stats_path);
+        ret |= rkaiq_get_devname(device, "rkisp_mainpath",
+                                media_info->mainpath);
+    }
+    if (ret) {
+        fprintf(stderr, "Cound not find rkisp dev names, skipped %s\n", mdev_path);
+        media_device_unref (device);
+        return ret;
+    }
 
-	ret = rkaiq_enumrate_modules(device, media_info);
-	media_device_unref (device);
+    sensor_name = rk_aiq_uapi_sysctl_getBindedSnsEntNmByVd(media_info->mainpath);
+    if (sensor_name == NULL || strlen(sensor_name) == 0) {
+        fprintf(stderr, "ERR: No sensor attached to %s\n", mdev_path);
+        media_device_unref (device);
+        return -EINVAL;
+    }
 
-	return ret;
+    strcpy(media_info->sensor_entity_name, sensor_name);
+
+    media_device_unref (device);
+
+    return ret;
 }
 
 
@@ -203,11 +135,9 @@ static void init_engine(void)
 {
     int index;
 
-    for (index=0; index<RKAIQ_CAMS_NUM_MAX; index++)
-        if(media_info.cams[index].link_enabled)
-            break;
+    aiq_ctx = rk_aiq_uapi_sysctl_init(media_info.sensor_entity_name, IQ_PATH, NULL, NULL);
 
-	aiq_ctx = rk_aiq_uapi_sysctl_init(media_info.cams[index].sensor_entity_name, NULL, NULL, NULL);
+    rk_aiq_uapi_sysctl_setMulCamConc(aiq_ctx, 1);
 
     if (rk_aiq_uapi_sysctl_prepare(aiq_ctx, width, height, RK_AIQ_WORKING_MODE_NORMAL)) {
         ERR("rkaiq engine prepare failed !\n");
@@ -246,16 +176,15 @@ static int wait_stream_event(int fd, unsigned int event_type, int time_out_ms)
     CLEAR(event);
 
     do {
-	/*
-	 * xioctl instead of poll.
-	 * Since poll() cannot wait for input before stream on,
-	 * it will return an error directly. So, use ioctl to
-	 * dequeue event and block until sucess.
-	 */
-	ret = xioctl(fd, VIDIOC_DQEVENT, &event);
-	if (ret == 0 && event.type == event_type) {
-		return 0;
-	}
+        /*
+         * xioctl instead of poll.
+         * Since poll() cannot wait for input before stream on,
+         * it will return an error directly. So, use ioctl to
+         * dequeue event and block until sucess.
+         */
+        ret = xioctl(fd, VIDIOC_DQEVENT, &event);
+        if (ret == 0 && event.type == event_type)
+            return 0;
     } while (true);
 
     return -1;
@@ -338,7 +267,7 @@ void parse_args(int argc, char **argv)
    if (!mdev_path) {
         ERR("argument --mmedia is required\n");
         exit(-1);
-	}
+    }
 }
 
 int main(int argc, char **argv)
