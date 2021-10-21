111bc4e47644d69d842dfaa4e9198112c73f5db4
Shunqian Zheng <zhengsq@rock-chips.com>
2021-10-21 18:24:07 +0800

    rkaiq_3A_server: start engine in threads
    Start one thread for every isp media device.
    This is because we shall check how many isp devices is availabe. If
    there're multiple isp devices, then this routine:
    
      rk_aiq_uapi_sysctl_setMulCamConc(aiq_ctx, 1);
    
    shall be called before ...sysctl_prepare().
    
    In a single process, we're good to control ..._setMulCamConc().
    
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
    Change-Id: Ic4966795c6823e41f05933a56cc17bfb071b91db

--- rkaiq_3A_server/rkaiq_3A_server.cpp
+++ rkaiq_3A_server/rkaiq_3A_server.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <pthread.h>
 
 #include <linux/videodev2.h>
 #include "rk_aiq_user_api_sysctl.h"
@@ -26,13 +27,15 @@
 #define RKAIQ_FILE_PATH_LEN                       64
 #define RKAIQ_FLASH_NUM_MAX                       2
 
+/* 1 vicap + 2 mipi + 1 bridge + 1 redundance */
+#define MAX_MEDIA_NODES                           5
+
 #define IQ_PATH "/etc/iqfiles/"
 
-rk_aiq_sys_ctx_t* aiq_ctx = NULL;
 static int silent = 0;
 static int width = 2688;
 static int height = 1520;
-static const char *mdev_path = NULL;
+static int has_mul_cam = 0;
 
 struct rkaiq_media_info {
     char sd_isp_path[RKAIQ_FILE_PATH_LEN];
@@ -40,10 +43,15 @@ struct rkaiq_media_info {
     char vd_stats_path[RKAIQ_FILE_PATH_LEN];
     char mainpath[RKAIQ_FILE_PATH_LEN];
     char sensor_entity_name[32];
+
+    char mdev_path[32];
+    int available;
+    rk_aiq_sys_ctx_t* aiq_ctx;
+
+    pthread_t pid;
 };
 
-static struct rkaiq_media_info media_info;
-
+static struct rkaiq_media_info media_infos[MAX_MEDIA_NODES];
 
 static void errno_exit(const char *s)
 {
@@ -86,13 +94,13 @@ static int rkaiq_get_devname(struct media_device *device, const char *name, char
     return 0;
 }
 
-int rkaiq_get_media_info(struct rkaiq_media_info *media_info, const char *mdev_path) 
+int rkaiq_get_media_info(struct rkaiq_media_info *media_info)
 {
     struct media_device *device = NULL;
     const char *sensor_name;
     int ret;
 
-    device = media_device_new (mdev_path);
+    device = media_device_new (media_info->mdev_path);
     if (!device)
         return -ENOMEM;
     /* Enumerate entities, pads and links. */
@@ -111,14 +119,14 @@ int rkaiq_get_media_info(struct rkaiq_media_info *media_info, const char *mdev_p
                                 media_info->mainpath);
     }
     if (ret) {
-        fprintf(stderr, "Cound not find rkisp dev names, skipped %s\n", mdev_path);
+        fprintf(stderr, "Cound not find rkisp dev names, skipped %s\n", media_info->mdev_path);
         media_device_unref (device);
         return ret;
     }
 
     sensor_name = rk_aiq_uapi_sysctl_getBindedSnsEntNmByVd(media_info->mainpath);
     if (sensor_name == NULL || strlen(sensor_name) == 0) {
-        fprintf(stderr, "ERR: No sensor attached to %s\n", mdev_path);
+        fprintf(stderr, "ERR: No sensor attached to %s\n", media_info->mdev_path);
         media_device_unref (device);
         return -EINVAL;
     }
@@ -130,26 +138,27 @@ int rkaiq_get_media_info(struct rkaiq_media_info *media_info, const char *mdev_p
     return ret;
 }
 
-
-static void init_engine(void)
+static void init_engine(struct rkaiq_media_info *media_info)
 {
     int index;
 
-    aiq_ctx = rk_aiq_uapi_sysctl_init(media_info.sensor_entity_name, IQ_PATH, NULL, NULL);
+    media_info->aiq_ctx = rk_aiq_uapi_sysctl_init(media_info->sensor_entity_name,
+                                                  IQ_PATH, NULL, NULL);
+    if (has_mul_cam)
+        rk_aiq_uapi_sysctl_setMulCamConc(media_info->aiq_ctx, 1);
 
-    rk_aiq_uapi_sysctl_setMulCamConc(aiq_ctx, 1);
-
-    if (rk_aiq_uapi_sysctl_prepare(aiq_ctx, width, height, RK_AIQ_WORKING_MODE_NORMAL)) {
+    if (rk_aiq_uapi_sysctl_prepare(media_info->aiq_ctx,
+            width, height, RK_AIQ_WORKING_MODE_NORMAL)) {
         ERR("rkaiq engine prepare failed !\n");
         exit(-1);
     }
 }
 
-static void start_engine(void)
+static void start_engine(struct rkaiq_media_info *media_info)
 {
     DBG("device manager start\n");
-    rk_aiq_uapi_sysctl_start(aiq_ctx );
-    if (aiq_ctx == NULL) {
+    rk_aiq_uapi_sysctl_start(media_info->aiq_ctx);
+    if (media_info->aiq_ctx == NULL) {
         ERR("rkisp_init engine failed\n");
         exit(-1);
     } else {
@@ -157,14 +166,14 @@ static void start_engine(void)
     }
 }
 
-static void stop_engine(void)
+static void stop_engine(struct rkaiq_media_info *media_info)
 {
-    rk_aiq_uapi_sysctl_stop(aiq_ctx, false);
+    rk_aiq_uapi_sysctl_stop(media_info->aiq_ctx, false);
 }
 
-static void deinit_engine(void)
+static void deinit_engine(struct rkaiq_media_info *media_info)
 {
-    rk_aiq_uapi_sysctl_deinit(aiq_ctx);
+    rk_aiq_uapi_sysctl_deinit(media_info->aiq_ctx);
 }
 
 // blocked func
@@ -191,7 +200,7 @@ static int wait_stream_event(int fd, unsigned int event_type, int time_out_ms)
 
 }
 
-static int subscrible_stream_event(int fd, bool subs)
+static int subscrible_stream_event(struct rkaiq_media_info *media_info, int fd, bool subs)
 {
     struct v4l2_event_subscription sub;
     int ret = 0;
@@ -202,7 +211,7 @@ static int subscrible_stream_event(int fd, bool subs)
                  subs ? VIDIOC_SUBSCRIBE_EVENT : VIDIOC_UNSUBSCRIBE_EVENT,
                  &sub);
     if (ret) {
-        ERR("can't subscribe %s start event!\n", media_info.vd_params_path);
+        ERR("can't subscribe %s start event!\n", media_info->vd_params_path);
         exit(EXIT_FAILURE);
     }
 
@@ -212,10 +221,10 @@ static int subscrible_stream_event(int fd, bool subs)
                  subs ? VIDIOC_SUBSCRIBE_EVENT : VIDIOC_UNSUBSCRIBE_EVENT,
                  &sub);
     if (ret) {
-        ERR("can't subscribe %s stop event!\n", media_info.vd_params_path);
+        ERR("can't subscribe %s stop event!\n", media_info->vd_params_path);
     }
 
-    DBG("subscribe events from %s success !\n", media_info.vd_params_path);
+    DBG("subscribe events from %s success !\n", media_info->vd_params_path);
 
     return 0;
 }
@@ -229,20 +238,16 @@ void parse_args(int argc, char **argv)
        int this_option_optind = optind ? optind : 1;
        int option_index = 0;
        static struct option long_options[] = {
-           {"mmedia",    required_argument, 0, 'm' },
            {"silent",    no_argument,       0, 's' },
            {"help",      no_argument,       0, 'h' },
            {0,           0,                 0,  0  }
        };
 
-       c = getopt_long(argc, argv, "m:sh", long_options, &option_index);
+       c = getopt_long(argc, argv, "sh", long_options, &option_index);
        if (c == -1)
            break;
 
        switch (c) {
-       case 'm':
-           mdev_path = optarg;
-           break;
        case 'w':
            width = atoi(optarg);
            break;
@@ -254,7 +259,6 @@ void parse_args(int argc, char **argv)
            break;
        case '?':
            ERR("Usage: %s to start 3A engine\n"
-               "         --mmedia,  optional, mapped media device node\n"
                "         --silent,  optional, subpress debug log\n",
                argv[0]);
            exit(-1);
@@ -263,18 +267,51 @@ void parse_args(int argc, char **argv)
            ERR("?? getopt returned character code %c ??\n", c);
        }
    }
-
-   if (!mdev_path) {
-        ERR("argument --mmedia is required\n");
-        exit(-1);
-    }
 }
 
-int main(int argc, char **argv)
+void *engine_thread(void *arg)
 {
     int ret = 0;
     int isp_fd;
     unsigned int stream_event = -1;
+    struct rkaiq_media_info *media_info;
+
+    media_info = (struct rkaiq_media_info *) arg;
+
+    isp_fd = open(media_info->vd_params_path, O_RDWR);
+    if (isp_fd < 0) {
+        ERR("open %s failed %s\n", media_info->vd_params_path, strerror(errno));
+        return NULL;
+    }
+
+    subscrible_stream_event(media_info, isp_fd, true);
+    init_engine(media_info);
+
+    for (;;) {
+        DBG("%s: wait stream start event...\n", media_info->mdev_path);
+        wait_stream_event(isp_fd, CIFISP_V4L2_EVENT_STREAM_START, -1);
+        DBG("%s: wait stream start event success ...\n", media_info->mdev_path);
+
+        start_engine(media_info);
+
+        DBG("%s: wait stream stop event...\n", media_info->mdev_path);
+        wait_stream_event(isp_fd, CIFISP_V4L2_EVENT_STREAM_STOP, -1);
+        DBG("%s: wait stream stop event success ...\n", media_info->mdev_path);
+
+        stop_engine(media_info);
+    }
+
+    deinit_engine(media_info);
+    subscrible_stream_event(media_info, isp_fd, false);
+    close(isp_fd);
+
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    int ret, i;
+    int threads = 0;
 
     /* Line buffered so that printf can flash every line if redirected to
      * no-interactive device.
@@ -282,39 +319,36 @@ int main(int argc, char **argv)
     setlinebuf(stdout);
 
     parse_args(argc, argv);
-    for (;;) {
-        /* Refresh media info so that sensor link status updated */
-        if (rkaiq_get_media_info(&media_info, mdev_path))
-            errno_exit("Bad media topology\n");
 
-        isp_fd = open(media_info.vd_params_path, O_RDWR);
-        if (isp_fd < 0) {
-            ERR("open %s failed %s\n", media_info.vd_params_path,
-                strerror(errno));
-            exit(-1);
+    for (i = 0; i < MAX_MEDIA_NODES; i++) {
+        sprintf(media_infos[i].mdev_path, "/dev/media%d", i);
+        if (rkaiq_get_media_info(&media_infos[i])) {
+            ERR("Bad media topology for: %s\n", media_infos[i].mdev_path);
+            media_infos[i].available = 0;
+            continue;
         }
-        subscrible_stream_event(isp_fd, true);
-        init_engine();
+        media_infos[i].available = 1;
+        threads++;
+    }
 
-        {
-            DBG("wait stream start event...\n");
-            wait_stream_event(isp_fd, CIFISP_V4L2_EVENT_STREAM_START, -1);
-            DBG("wait stream start event success ...\n");
+    if (threads > 1)
+        has_mul_cam = 1;
 
-            start_engine();
-
-            DBG("wait stream stop event...\n");
-            wait_stream_event(isp_fd, CIFISP_V4L2_EVENT_STREAM_STOP, -1);
-            DBG("wait stream stop event success ...\n");
-
-            stop_engine();
+    for (i = 0; i < MAX_MEDIA_NODES; i++) {
+        if (!media_infos[i].available)
+            continue;
+        ret = pthread_create(&media_infos[i].pid, NULL, engine_thread, &media_infos[i]);
+        if (ret) {
+            media_infos[i].pid = 0;
+            ERR("Failed to create camera engine thread for: %s\n", media_infos[i].mdev_path);
+            errno_exit("Create thread failed");
         }
+    }
 
-        deinit_engine();
-        subscrible_stream_event(isp_fd, false);
-        close(isp_fd);
-
-        DBG("----------------------------------------------\n\n");
+    for (i = 0; i < MAX_MEDIA_NODES; i++) {
+        if (!media_infos[i].available || media_infos[i].pid == 0)
+            continue;
+        pthread_join(media_infos[i].pid, NULL);
     }
 
     return 0;
