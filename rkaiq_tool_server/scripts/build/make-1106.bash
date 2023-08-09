064afd2fcdc3d4171705949721f9a681743f1566
willam.wei <willam.wei@rock-chips.com>
2023-08-09 09:27:20 +0800

    rkaiq_tool_server: modify file permisson
    Signed-off-by: willam.wei <willam.wei@rock-chips.com>
    Change-Id: Ic9747fdacd8ec2d77a6b85c4f12455adfe210ad2

--- rkaiq_tool_server/scripts/build/make-1106.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cmake -D CMAKE_C_COMPILER=${PWD}/../../../../tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc \
--D CMAKE_CXX_COMPILER=${PWD}/../../../../tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ \
-CMakeLists.txt
-
